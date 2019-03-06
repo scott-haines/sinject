93820714c4a62043d3243ac7d8a767e2a238ad5e
Haines, Scott <scott.haines@londonlife.com>
2019-03-06 16:02:16 -0500

    some lumber support and new flags for inject command

--- commands/inject.go
+++ commands/inject.go
@@ -9,16 +9,21 @@ import (
 
 var (
 	injectCmd = &cobra.Command{
-		Use:   "inject",
+		Use:   "inject [OPTIONS]  FILE [FILE...]",
 		Short: "Inject secrets into target file",
-
-		Run: inject,
+		Run:   inject,
 	}
-	file string
+	file        string
+	prescanmode string
+	secretspath string
+	token       string
 )
 
 func init() {
-	injectCmd.Flags().StringVarP(&file, "file", "f", "", "The file to inject secrets into")
+	injectCmd.Flags().StringVar(&file, "file", "", "The file to inject secrets into")
+	injectCmd.Flags().StringVar(&prescanmode, "pre-scan-mode", "secret", "Sets the pre-scan mode.\n\nnone - sinject will not perform pre-scanning.\nsecret - sinject will output errors if there is no secret for a discovered token.\ntoken - sinject will output errors if a secret is present but there is no token.\nfull - both secret + token\n\n")
+	injectCmd.Flags().StringVar(&secretspath, "secrets-path", "/run/secrets", "The path to the directory containing the secrets.")
+	injectCmd.Flags().StringVar(&token, "token", "%%%", "Sets the token wrapper.")
 }
 
 func inject(ccmd *cobra.Command, args []string) {
--- commands/root.go
+++ commands/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/jcelliott/lumber"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -19,7 +20,7 @@ var rootCmd = &cobra.Command{
 	Short:                 "secrets injection utility for containers",
 	Long:                  ``,
 	Version:               fmt.Sprintf("%s, build %s", version.Version, version.GitCommit),
-	DisableFlagsInUseLine: true,
+	DisableFlagsInUseLine: false,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -32,12 +33,42 @@ func Execute() {
 }
 
 func init() {
+	lumber.Trace("Initializing the root command.")
+
+	rootCmd.SetUsageTemplate(`Usage:{{if .Runnable}}
+  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [OPTIONS] COMMAND [ARG...]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+  {{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+{{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`)
+
 	// set config defaults
 	cobra.OnInitialize(initConfig)
+	verbosity := "INFO"
 
 	// cli flags
+	rootCmd.PersistentFlags().String("verbosity", verbosity, "Verbosity level of messages (TRACE, DEBUG, INFO, WARN, ERROR, FATAL)")
 
 	// bind config to cli flags
+	viper.BindPFlag("verbosity", rootCmd.PersistentFlags().Lookup("verbosity"))
 
 	// cli-only flags
 	rootCmd.Flags().Bool("version", false, "Print version information and quit")
@@ -59,9 +90,9 @@ func initConfig() {
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".cobra-example" (without extension).
+		// Search config in home directory with name ".sinject" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigName(".cobra-example")
+		viper.SetConfigName(".sinject")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
